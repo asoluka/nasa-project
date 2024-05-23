3e9103d81a7d361a66049a4aa54bac71f92cc0a9
Austin Asoluka <stracool9@gmail.com>
2024-05-23 01:49:43 +0100

    update api version

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require("path");
 const planetsRouter = require("./routes/planets/planets.router");
 const launchesRouter = require("./routes/launches/launches.router");
+const apiV1 = require("./apiV1");
 const app = express();
 
 app.use(
@@ -15,8 +16,7 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
+app.use("/v1", apiV1);
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
